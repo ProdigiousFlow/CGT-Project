0f5b242a4e9b00eaabd8e741e86a5717f344667f
ProdigiousFlow <heathoaks5@hotmail.com>
2022-04-27 14:56:54 -0400

    My CGT 141 Project

---
+++ JS/index.js
@@ -0,0 +1,39 @@
+function hover(Beginner) {
+    Beginner.setAttribute('src', 'images/Beginner.png');
+}
+
+function unhover(Beginner) {
+    Beginner.setAttribute('src', 'images/Beginner2.png');
+}
+
+function characterHover(Characters) {
+    Characters.setAttribute('src', 'images/Characters.png');
+}
+
+function characterUnhover(Characters) {
+    Characters.setAttribute('src', 'images/Characters2.png');
+}
+
+function discordHover(Discord) {
+    Discord.setAttribute('src', 'images/Discord.png');
+}
+
+function discordUnhover(Discord) {
+    Discord.setAttribute('src', 'images/Discord2.png');
+}
+
+// function hoverC(Characters) {
+//     Characters.setAttribute('src', 'images/Characters.png');
+// }
+
+// function unhoverC(Characters) {
+//     Characters.setAttribute('src', 'images/Characters2.png');
+// }
+
+// function hoverD(Discord) {
+//     Discord.setAttribute('src', 'images/Discord.png');
+// }
+
+// function unhoverD(Discord) {
+//     Discord.setAttribute('src', 'images/Discord2.png');
+// }
